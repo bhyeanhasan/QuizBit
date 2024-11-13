33406da5112c7e6a94411b7e4637b30632d400b6
Md. Babul Hasan (NoYoN) <bhyean16@cse.pstu.ac.bd>
2024-11-13 20:56:59 +0600

    database design complete

--- App_ExamManager/models.py
+++ App_ExamManager/models.py
@@ -3,7 +3,6 @@ from django.db import models
 from App_QuestionManager.models import Question, Option
 
 
-
 class Exam(models.Model):
     title = models.CharField(max_length=255)
     created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='exams')
@@ -20,11 +19,10 @@ class Enrollment(models.Model):
     student = models.ForeignKey(User, on_delete=models.CASCADE)
     exam = models.ForeignKey(Exam, on_delete=models.CASCADE)
     score = models.FloatField(default=0)
-    status = models.CharField(max_length=10, choices=[('passed', 'Passed'), ('failed', 'Failed')],default='failed')
+    status = models.CharField(max_length=10, choices=[('passed', 'Passed'), ('failed', 'Failed')], default='failed')
 
     def __str__(self):
         return f"{self.student.username} - {self.exam.title} Attempt"
-
 
 
 class AnswerScript(models.Model):
