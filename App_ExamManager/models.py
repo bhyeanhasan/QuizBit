cb89e4b557e150efd4b49a0ba5690f81e32a64a7
Md. Babul Hasan (NoYoN) <bhyean16@cse.pstu.ac.bd>
2024-11-13 20:54:36 +0600

    fresh start

--- App_ExamManager/models.py
+++ App_ExamManager/models.py
@@ -1,3 +1,36 @@
+from django.contrib.auth.models import User
 from django.db import models
+from App_QuestionManager.models import Question, Option
 
-# Create your models here.
+
+
+class Exam(models.Model):
+    title = models.CharField(max_length=255)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='exams')
+    duration = models.PositiveIntegerField(help_text="Duration in minutes")
+    total_marks = models.PositiveIntegerField()
+    passing_marks = models.PositiveIntegerField()
+    start_time = models.DateTimeField(blank=True, null=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Enrollment(models.Model):
+    student = models.ForeignKey(User, on_delete=models.CASCADE)
+    exam = models.ForeignKey(Exam, on_delete=models.CASCADE)
+    score = models.FloatField(default=0)
+    status = models.CharField(max_length=10, choices=[('passed', 'Passed'), ('failed', 'Failed')],default='failed')
+
+    def __str__(self):
+        return f"{self.student.username} - {self.exam.title} Attempt"
+
+
+
+class AnswerScript(models.Model):
+    enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    selected_option = models.ForeignKey(Option, on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.enrollment.student.username}'s answer for {self.question.text}"
--- App_QuestionManager/models.py
+++ App_QuestionManager/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from App_ExamManager.models import Exam
 
-# Create your models here.
+
+class Question(models.Model):
+    exam = models.ForeignKey(Exam, on_delete=models.CASCADE)
+    text = models.TextField()
+    marks = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return f"Question {self.id} for {self.exam.title}"
+
+
+class Option(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    text = models.CharField(max_length=255)
+    is_correct = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Option {self.text} for {self.question.id}"
